a9f6521c8c0f24ed62e9c3b50046b33df31520ef
Didji <ngc_master@hotmail.fr>
2020-12-05 01:58:26 +0100

    :recycle: Refactor pig-latin code

--- exo_pig-latin/src/main.rs
+++ exo_pig-latin/src/main.rs
@@ -1,19 +1,30 @@
+static VOWELS: [char; 6] = ['a', 'e', 'i', 'o', 'u', 'y'];
+
 fn main() {
     let input_string = String::from("Brrrr the mail comes in three batches per day and the square peg will settle in the round hole");
+
+    println!("{}", transform_input_string(input_string));
+}
+
+fn transform_input_string(input_string: String) -> String {
     let mut output: Vec<String> = vec![];
-    let vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
 
     for word in input_string.split_whitespace() {
-        let mut lower_word = word.to_lowercase();
-        let starts_with_vowel = vowels.iter().any(|&v| lower_word.starts_with(v));
-        if starts_with_vowel {
-            output.push(append_suffix(&mut lower_word, "hay"));
-        } else {
-            output.push(pig_consonant(&mut lower_word, &vowels));
-        }
+        output.push(transform_word(&mut word.to_lowercase()));
     }
+    output.join(" ")
+}
 
-    println!("{}", output.join(" "));
+fn transform_word(word: &mut String) -> String {
+    if starts_with_vowel(word) {
+        return append_suffix(word, "hay")
+    } else {
+        return pig_consonant(word)
+    }
+}
+
+fn starts_with_vowel(word: &String) -> bool {
+    VOWELS.iter().any(|&v| word.starts_with(v))
 }
 
 fn append_suffix(word: &mut String, append: &str) -> String {
@@ -21,19 +32,25 @@ fn append_suffix(word: &mut String, append: &str) -> String {
     word.to_string()
 }
 
-fn pig_consonant(word: &mut String, vowels: &[char; 6]) -> String {
+fn pig_consonant(word: &mut String) -> String {
     let mut new_word = word.clone();
-    // Finds the position of the first vowel in the word
-    let first_vowel = vowels
+    let first_vowel = find_position_of_first_vowel(word);
+    if let Some(position) = first_vowel {
+        new_word = move_first_consonants_to_end(word, position);
+    }
+    append_suffix(&mut new_word, "ay")
+}
+
+fn find_position_of_first_vowel(word: &String) -> Option<usize> {
+    VOWELS
         .iter()
         .map(|&v| word.find(v))
         .into_iter()
         .flat_map(|v| v)
-        .min();
-    // If the word contains a vowel, we move all the consonants before it to the end of the word
-    if let Some(pos) = first_vowel {
-        let after_vowel = word.split_off(pos);
-        new_word = format!("{}{}", after_vowel, word);
-    }    
-    append_suffix(&mut new_word, "ay")
+        .min()
+}
+
+fn move_first_consonants_to_end(word: &mut String, position_of_first_vowel: usize) -> String {
+    let after_vowel = word.split_off(position_of_first_vowel);
+    format!("{}{}", after_vowel, word)
 }
